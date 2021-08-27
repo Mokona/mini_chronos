c8e41d7517135ce984258d6e6ce83eb34681ea2c
Sylvain Glaize <mokona@puupuu.org>
2021-08-27 21:38:36 +0200

    The system can start a timer.
    Not doing any measurement at the moment.

--- src/mini_chronos.h
+++ src/mini_chronos.h
@@ -1,1 +1,20 @@
-// Mini Chronos
+#include <string>
+#include <vector>
+
+namespace MiniChronos
+{
+    // Move this in a private header to keep the interface simple
+    class Database
+    {
+    public:
+        bool has_path(const std::string& path);
+        std::size_t ensures_key(std::string& key);
+
+    private:
+        std::vector<std::string> all_paths;
+    };
+
+    void init(Database& db);
+
+    void start(std::string&& timer_id);
+}
--- tests/mini_chronos_tests.cpp
+++ tests/mini_chronos_tests.cpp
@@ -3,3 +3,15 @@
 #include "gmock/gmock.h"
 
 using namespace testing;
+
+TEST(MiniChronos, start_a_timer_references_it_in_the_db)
+{
+    MiniChronos::Database db;
+    MiniChronos::init(db);
+    MiniChronos::start("timer_1");
+
+    ASSERT_TRUE(db.has_path("timer_1"));
+    ASSERT_FALSE(db.has_path("timer_2"));
+}
+
+// Cannot be initialized twice
